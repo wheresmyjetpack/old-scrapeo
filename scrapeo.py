af0ab2d1148d866dcabb49098778a7442860dd43
Paul <wheresmyjetpack03@gmail.com>
2016-05-07 21:18:15 -0500

    added --title and --description flags

--- scrapeo.py
+++ scrapeo.py
@@ -7,10 +7,15 @@ from bs4 import BeautifulSoup
 
 
 @click.command()
-@click.option('--url',
-        help='URL for scrapeo to scrape')
-def cli(url):
-    """ Scrape data from webpages that are useful for SEO purposes """
+@click.option('--title', is_flag='true',
+        help='Tell scrapeo to print the title of the document.'
+        )
+@click.option('--description', is_flag='true',
+        help='Tell scrapeo to print the meta description of the document.'
+        )
+@click.argument('url')
+def cli(title, description, url):
+    """ Scrape data from a document found at URL for SEO data analysis """
 
     try:
         req = requests.get(url)
@@ -20,7 +25,16 @@ def cli(url):
 
         html = req.text
         soup = BeautifulSoup(html, 'html.parser')
-        click.echo(soup.title)
+
+        if title:
+            _title = soup.title.text.encode('utf-8')
+            _title_out = 'Title: %s' % _title
+            click.echo(_title_out)
+
+        if description:
+            _description = soup.find('meta', { 'name': 'description' })
+            _description_out = 'Description: %s' % _description['content'].encode('utf-8')
+            click.echo(_description_out)
 
     except requests.exceptions.RequestException, e:
         for arg in e.args:
