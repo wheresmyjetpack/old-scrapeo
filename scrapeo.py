96eadf712de85af1a69a799a6f94786933477b8d
Paul <wheresmyjetpack03@gmail.com>
2016-05-07 23:43:13 -0500

    added h1 flag, modified title and description flag

--- scrapeo.py
+++ scrapeo.py
@@ -13,8 +13,11 @@ from bs4 import BeautifulSoup
 @click.option('--description', is_flag='true',
         help='Tell scrapeo to print the meta description of the document.'
         )
+@click.option('--h1', is_flag='true',
+        help='Tell scrapeo to print the text node for all h1\'s in the document'
+        )
 @click.argument('url')
-def cli(title, description, url):
+def cli(title, description, h1, url):
     """ Scrape data from a document found at URL for SEO data analysis """
 
     try:
@@ -26,15 +29,29 @@ def cli(title, description, url):
         html = req.text
         soup = BeautifulSoup(html, 'html.parser')
 
+        # Flags
         if title:
-            _title = soup.title.text.encode('utf-8')
-            _title_out = 'Title: %s' % _title
-            click.echo(_title_out)
+            try:
+                _title = soup.title.text.encode('utf-8')
+                _title_out = 'Title: %s' % _title
+                click.echo(_title_out)
+            except AttributeError:
+                click.echo('Document contains no title tag')
 
         if description:
             _description = soup.find('meta', { 'name': 'description' })
-            _description_out = 'Description: %s' % _description['content'].encode('utf-8')
-            click.echo(_description_out)
+
+            try:
+                _description_out = 'Description: %s' % _description['content'].encode('utf-8')
+                click.echo(_description_out)
+            except TypeError:
+                click.echo('Document contains no meta description')
+
+        if h1:
+            _h1s = soup.find_all('h1')
+            click.echo('h1\'s:')
+            for _h1 in _h1s:
+                click.echo(_h1.text.encode('utf-8').strip())
 
     except requests.exceptions.RequestException, e:
         for arg in e.args:
